40ee4fd2d3f47e221efa7b3e0e37e4dcfe4a2a75
Gisomar <gisomarjr1994@gmail.com>
2015-08-26 20:31:51 -0300

    commit cores das tabelas e outros ajustes

--- app/src/main/java/br/com/laboratorio/hemope/View/SlidingTabStrip.java
+++ app/src/main/java/br/com/laboratorio/hemope/View/SlidingTabStrip.java
@@ -28,9 +28,9 @@ import android.widget.LinearLayout;
 
 class SlidingTabStrip extends LinearLayout {
 
-    private static final int DEFAULT_BOTTOM_BORDER_THICKNESS_DIPS = 0;
+    private static final int DEFAULT_BOTTOM_BORDER_THICKNESS_DIPS = 4;
     private static final byte DEFAULT_BOTTOM_BORDER_COLOR_ALPHA = 0x26;
-    private static final int SELECTED_INDICATOR_THICKNESS_DIPS = 3;
+    private static final int SELECTED_INDICATOR_THICKNESS_DIPS = 6;
     private static final int DEFAULT_SELECTED_INDICATOR_COLOR = 0xFFE91E63;
 
     private final int mBottomBorderThickness;
